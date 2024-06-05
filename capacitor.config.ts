3c0c2c1bffd0b1faec7409a9a9cec43ce0425b7d
JOMG10 <josemartinezgenaro5@gmail.com>
2024-06-05 03:35:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppMultiplatarfoma',
+  webDir: 'dist'
+};
+
+export default config;
